c73802fcb2d2f18c250b604e4f6c6935bb450268
kenzabdulrazick <139810886+kenzabdulrazick@users.noreply.github.com>
2023-07-25 09:54:55 +0530

    Add files via upload

---
+++ Fibinonacii.java
@@ -0,0 +1,16 @@
+package kenzie;
+//HOME ASSINGMENT 1
+public class Fibinonacii {
+ public static void main(String args[]) {
+	 int a=0;
+	 int b=1;
+	 int c;
+	 for (int i=0; i<13;i++) {
+		 System.out.print(a+ "   ");
+		 c=a+b;
+		 a=b;
+		 b=c;
+	 }
+	 
+ }
+}
---
+++ IsPrime.java
@@ -0,0 +1,25 @@
+package kenzie;
+
+public class IsPrime {
+	public static void main(String args[]) {
+		int n=13;
+		for(int i=2;i<n;i++) {
+			if(n%i==0) {
+			
+				System.out.println(" is prime"  );
+			}
+			else { 
+				System.out.println("  is not prime" );
+			}
+			break;
+
+		}
+		
+	}
+}
+		
+			
+		
+	
+
+
